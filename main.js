c4892383cb63038f527f8f0da1c71d1abe1a6777
Nayan Pahuja <nayanpahuja25@gmail.com>
2023-07-25 20:58:19 +0530

    Added final touches and styling

--- main.js
+++ main.js
@@ -26,6 +26,16 @@ const stunServers = {
     ]
 };
 
+
+let constraints = {
+    video:{
+        width:{min:640,ideal:1920,max:1920},
+        height: {min:480,ideal:1080,max:1080}
+
+    },
+    audio: true
+}
+
 let init = async () => {
     client = await AgoraRTM.createInstance(APP_ID)
     await client.login({uid, token})
@@ -36,7 +46,7 @@ let init = async () => {
     channel.on('MemberJoined', handleUserJoined)
     client.on('MessageFromPeer', handleMessageFromPeer)
     channel.on('MemberLeft',handleUserLeft)
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
         document.getElementById("user-1").srcObject = localStream;
 }
 
